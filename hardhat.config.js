a60acd369bfe7665ce97a38320a122161fe4b18b
faridanang <f4r1danangs@gmail.com>
2024-04-11 15:34:08 +0700

    commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,10 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    ganache: {
+      url: "http://127.0.0.1:7545",
+      chainId: 1337,
+    }
+  }
 };
--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,174 @@
-import Image from "next/image";
+'use client'
+
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import { BrowserProvider, Contract } from 'ethers';
+import Input from '@mui/material/Input';
+import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, getKeyValue } from "@nextui-org/react";
+
+
+
+const abi = [
+  {
+    "inputs": [
+      {
+        "internalType": "string",
+        "name": "_text",
+        "type": "string"
+      }
+    ],
+    "name": "addTodo",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "_id",
+        "type": "uint256"
+      }
+    ],
+    "name": "changeStatus",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "getLists",
+    "outputs": [
+      {
+        "components": [
+          {
+            "internalType": "string",
+            "name": "text",
+            "type": "string"
+          },
+          {
+            "internalType": "enum TodoList.Status",
+            "name": "status",
+            "type": "uint8"
+          },
+          {
+            "internalType": "uint256",
+            "name": "time",
+            "type": "uint256"
+          }
+        ],
+        "internalType": "struct TodoList.List[]",
+        "name": "",
+        "type": "tuple[]"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
+]
+const contratcAddress = "0x43a1EE282cdc8FE3e242e613181EB7a17ec2ff92"
+const privateKey = "0x822e2c5c9887c8e542bba331ba43408cca597449ccee1b2df9acfd8b13d9944c";
 
 export default function Home() {
+  const [account, setAccount] = React.useState();
+  const [datas, setDatas] = React.useState();
+  const [text, setText] = React.useState("");
+  const [selectedKeys, setSelectedKeys] = React.useState(new Set(["2"]));
+
+  const statusMap = {
+    0: 'Pending',
+    1: 'Completed',
+    // tambahkan pemetaan lain sesuai kebutuhan
+  };
+
+  const connectWallet = async () => {
+    try {
+      if (window.ethereum) {
+        const provider = new BrowserProvider(window.ethereum);
+        const signer = await provider.getSigner()
+        const contract = new Contract(contratcAddress, abi, signer);
+        let data = await contract.getLists();
+        data = data.map(entry => ({
+          text: entry[0],
+          status: entry[1],
+          time: entry[2]
+        }));
+
+        setAccount(await signer.getAddress())
+        setDatas(data);
+      } else {
+        return console.log("install metamask");
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const addTodo = async () => {
+    try {
+      // const provider = new JsonRpcProvider("http://127.0.0.1:7545");
+      const provider = new BrowserProvider(window.ethereum);
+      const signer = await provider.getSigner()
+      // const signer = new Wallet(privateKey, provider);
+      const contract = new Contract(contratcAddress, abi, signer);
+      const tx = await contract.addTodo(text);
+      await tx.wait();
+      window.location.reload()
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const changeStatusLol = async (i) => {
+    try {
+      const provider = new BrowserProvider(window.ethereum);
+      const signer = await provider.getSigner();
+      const contract = new Contract(contratcAddress, abi, signer);
+      const tx = await contract.changeStatus(i);
+      await tx.wait();
+      window.location.reload();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  React.useEffect(() => {
+    connectWallet();
+  }, [])
+
+  console.log(datas)
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className='max-w-xl w-full mx-auto'>
+      <div className='flex items-center justify-between'>
+        <h1 className='my-8 text-red-400 font-bold text-2xl text-center'>Todo Decentrilzed App</h1>
+        <Button variant="contained" disableElevation>
+          {account ? account?.slice(0, 10) + ". ." : <span onClick={connectWallet}>Connect</span>}
+        </Button>
+      </div>
+      <div>
+        <div className='bg-slate-200 max-w-[19rem] flex justify-between px-4 items-center rounded-md my-4'>
+          <Input size="sm" type="text" onChange={(e) => setText(e.target.value)} placeholder='Add Todo Here..' className='my-4' />
+          <button onClick={() => addTodo()} className='bg-slate-500 px-2 py-1 rounded-sm text-white font-bold'>Submit</button>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <Table removeWrapper aria-label="Example static collection table">
+        <TableHeader>
+          <TableColumn>Text</TableColumn>
+          <TableColumn>Time</TableColumn>
+          <TableColumn>Status</TableColumn>
+          <TableColumn>Action</TableColumn>
+        </TableHeader>
+        <TableBody>
+          {datas?.map((item, i) => (
+            <TableRow key={i}>
+              <TableCell>{item.text}</TableCell>
+              <TableCell>{new Date(Number(item.time) * 1000).toLocaleString()}</TableCell>
+              <TableCell>{Number(item.status) == 0 ? <span className='bg-red-500 px-2 py-1 rounded-sm text-white font-bold'>Pending</span> : <span className='bg-green-500 px-2 py-1 rounded-sm text-white font-bold'>Finished</span>}</TableCell>
+              <TableCell> <button onClick={() => changeStatusLol(i)} className='bg-green-500 px-2 py-1 rounded-sm text-white font-bold'>Checklist</button></TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
   );
 }
